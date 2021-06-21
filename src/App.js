9da06f5561c1898933a6150e551c48551f1575bb
Haji Russell <hajirussell@gmail.com>
2021-06-21 10:05:40 -0400

    another change added to app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This is the other feature component.</p>
+        <p>This is the other feature component. Another Change.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
